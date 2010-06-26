a9facd585d615eafdf8618ea3f531e4f635f1993
John Harrison <ash@greaterthaninfinity.com>
2010-06-26 14:27:05 -0500

    Adding an outline for the chapters, more shell changes, and about to start writing the actual tutorials

---
+++ frontend/data/chapters/1.js
@@ -0,0 +1,12 @@
+{
+    'title': 'Basics - Math Infix',
+    'steps': [
+                {
+                    'example': '1 + 2',
+                    'explanation': 'Lets begin with a basic addition operation'
+                },
+                {
+                    'example': ''
+                }
+             ]
+}
