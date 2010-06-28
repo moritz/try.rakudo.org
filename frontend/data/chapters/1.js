8513a5470ae1bbe72c2bc68cb238ff8cae16bcb8
John Harrison <ash@greaterthaninfinity.com>
2010-06-27 23:09:38 -0500

    Shell style changes and script updates for chapters.

--- frontend/data/chapters/1.js
+++ frontend/data/chapters/1.js
@@ -1,12 +1,9 @@
 {
-    'title': 'Basics - Math Infix',
-    'steps': [
+    "title": "Basics - Math Infix",
+    "steps": [
                 {
-                    'example': '1 + 2',
-                    'explanation': 'Lets begin with a basic addition operation'
-                },
-                {
-                    'example': ''
+                    "example": "1 + 2",
+                    "explanation": "Lets begin with a basic addition operation"
                 }
              ]
 }
--- frontend/data/chapters/index.js
+++ frontend/data/chapters/index.js
@@ -1,60 +1,115 @@
 {
     "title": "Chapter Index",
-    
-    "details": "
-    Chapter 1 - Math
-        * Basic Infix
-        * List Ops
-        * Advanced Ops
-    Chapter 2 - Variables
-        * Scalars
-        * Lists
-        * Scoping
-    Chapter 3 - Strings
-        * String Creation
-        * String Interolpation
-    Chapter 4 - Flow Control
-        * Looping Constructs, for, loop, while
-        * break, continue, next, last
-    Chapter 5 - Advanced Variables
-        * Hashes
-        * Subsets
-        * Types Constraints
-        * Static Typing
-        * Twigils
-    Chapter 6 - Functions
-        * Declarations 
-        * Prototypes
-        * Arguments
-    Chapter 7 - Regular Expressions
-        * Syntax
-        * Modifiers
-        * Explain Syntax differences from perl5 and pcre
-    Chapter 8 - Advanced Flow Control
-        * Phasers
-        * gather/take
-    Chapter 9 - Modules
-        * Namespaces
-        * Exporting
-    Chapter 10 - Classes
-        * Class definition
-        * method,
-     submethod's
-        * Object Construction
-    Chapter 11 - Advanced Functions
-        * Multi-methods
-        * Defining Custom Operators (eg. sub infix:<x> () { ... })
-    Chapter 12 - Exceptions 
-        * try { }
-        * CATCH { }
-    Chapter 13 - Roles
-        * Role definition
-        * does and but 
-        * Object Composition
-    Chapter 14 - Grammars
-        * Defintiion
-        * Tokens
-        * Rules
-    "
-
+    "info": [
+        {
+            "title": "Chapter 1 - Math",
+            "details": [
+                "Basic Infix",
+                "List Ops",
+                "Advanced Ops" 
+            ] 
+        },
+        {
+            "title": "Chapter 2 - Variables, Scalar Objects",
+            "details": [
+                "Scalars",
+                "Strings",
+                "Regex's as Scalars",
+                "Scoping" 
+            ] 
+        },
+        {
+            "title": "Chapter 3 - Containers",
+            "details": [
+                "Lists",
+                "Hases" 
+            ] 
+        },
+        {
+            "title": "Chapter 4 - Flow Control",
+            "details": [
+                "Looping Constructs: for, loop, while",
+                "break, continue, next, last" 
+            ] 
+        },
+        {
+            "title": "Chapter 5 - Regular Expressions",
+            "details": [
+                "Syntax",
+                "Modifiers",
+                "Explain Syntax differences from perl5 and pcre",
+                "Protoregex, LTM" 
+            ] 
+        },
+        {
+            "title": "Chapter 6 - Advanced Variables",
+            "details": [
+                "Hashes",
+                "Subsets",
+                "Types Constraints",
+                "Static Typing",
+                "Twigils" 
+            ] 
+        },
+        {
+            "title": "Chapter 7 - Functions",
+            "details": [
+                "Declarations ",
+                "Prototypes",
+                "Arguments" 
+            ] 
+        },
+        {
+            "title": "Chapter 8 - Advanced Flow Control",
+            "details": [
+                "Phasers",
+                "gather/take" 
+            ] 
+        },
+        {
+            "title": "Chapter 9 - Modules",
+            "details": [
+                "Namespaces",
+                "Exporting" 
+            ] 
+        },
+        {
+            "title": "Chapter 10 - Classes",
+            "details": [
+                "Class definition",
+                "method, submethods",
+                "Object Construction" 
+            ] 
+        },
+        {
+            "title": "Chapter 11 - Advanced Functions",
+            "details": [
+                "Multi-methods",
+                "Defining Custom Operators (eg. sub infix:<x> { ... })" 
+            ] 
+        },
+        {
+            "title": "Chapter 12 - Exceptions",
+            "details": [
+                "try { ... }",
+                "CATCH { ... }"
+            ] 
+        },
+        {
+            "title": "Chapter 13 - Roles",
+            "details": [
+                "Role definition",
+                "does and but ",
+                "Object Composition" 
+            ] 
+        },
+        {
+            "title": "Chapter 14 - Grammars",
+            "details": [
+                "Defintiion",
+                "Tokens",
+                "Rules" 
+            ] 
+        } 
+    ] 
 }
