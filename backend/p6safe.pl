34c0399f89700e26c3713accbf61894bd68b0cad
John Harrison <ash@greaterthaninfinity.com>
2010-09-01 14:08:23 -0500

    Getting a real safemode, that works, thanks to moritz_++

--- backend/p6safe.pl
+++ backend/p6safe.pl
@@ -1,15 +1,29 @@
-my $*ARGFILES = open '/home/john/Projects/try.rakudo.org/frontend/data/input_text.txt'; 
+my $*ARGFILES = open '/Users/john/Projects/try.rakudo.org/frontend/data/input_text.txt'; 
 
 module Safe { 
-    our sub forbidden(*@a, *%h) { die "Operation not permitted in safe mode" }; 
-    &GLOBAL::open   := &forbidden;
-    &GLOBAL::run    := &forbidden;
-    &GLOBAL::slurp  := &forbidden;
-    &GLOBAL::unlink := &forbidden;
-    &GLOBAL::dir    := &forbidden;
-    &GLOBAL::chdir  := &forbidden;
-    &GLOBAL::mkdir  := &forbidden;
-}
+    our sub forbidden(*@a, *%h) { die "Operation not permitted in safe mode" };
+    Q:PIR {
+        $P0 = get_hll_namespace
+        $P1 = get_hll_global ['Safe'], '&forbidden'
+        $P0['!qx']  = $P1
+        null $P1
+        set_hll_global ['IO'], 'Socket', $P1
+    };
+};
+Q:PIR {
+    .local pmc s
+    s = get_hll_global ['Safe'], '&forbidden'
+    $P0 = getinterp
+    $P0 = $P0['outer';'lexpad';1]
+    $P0['&run'] = s
+    $P0['&open'] = s
+    $P0['&slurp'] = s
+    $P0['&unlink'] = s
+    $P0['&dir'] = s
+    $P0['&chdir'] = s
+    $P0['&mkdir'] = s
+};
+
 use FORBID_PIR;
 
 Perl6::Compiler.interactive();
