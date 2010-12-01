266a4d10f4d0fc266d2b022aa173124c26dc4550
Anthony Parsons <ant@specialops.ath.cx>
2010-12-01 12:06:57 +0000

    Fix regex in chapter 2, put real code in 2/3 & 3/3

--- frontend/public/js/chapters/2.js
+++ frontend/public/js/chapters/2.js
@@ -3,17 +3,17 @@
     "steps": [
                 {
                     "example": "[+] 1..2",
-                    "match" : "[\\+]\\s*1\\.\\.2",
+                    "match" : "\\[\\+\\]\\s*1\\.\\.2",
                     "explanation": "List operators let you manipulate whole lists."
                 },
                 {
-                    "example" : "Foo",
-                    "match" : "Foo",
+                    "example" : "2 + 3 * 4 / 5",
+                    "match" : "2\\s*\\+\\s*3\\s*\\*\\s*4\\s*/\\s*5",
                     "explanation": "Multiply/divide are done before add/subtract"
                 },
                 {
-                    "example": "Bar",
-                    "match" : "Bar",
+                    "example": "(2 + 3) * 4 / 5",
+                    "match" : "\\(\\s*2\\s*\\+\\s*3\\s*\\)\\s*\\*\\s*4\\s*/\\s*5",
                     "explanation": "Parentheses are done before anything else"
                 }
              ]
